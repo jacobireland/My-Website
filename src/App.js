6740a511af532556301b0dc54e269beaefef5d85
Jacob Ireland <105519104+jacobireland@users.noreply.github.com>
2022-07-14 16:41:28 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import { Navbar, Header, About, Skills, Projects, Footer, } from './components';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="main_background">
+        <Navbar />
+        <Header />
+      </div>
+      <About />
+      <div className="skills_page">
+        <Skills />
+      </div>
+      <div className="projects_page">
+        <Projects />
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
