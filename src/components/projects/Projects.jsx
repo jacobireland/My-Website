d513f85b7a4af06ced53492c8ae1d600c048f8f2
Jacob Ireland <105519104+jacobireland@users.noreply.github.com>
2022-07-19 21:32:18 -0600

    Add files via upload

---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './projects.css';
+import spotifyflic from '../../assets/SpotiyAPIFlic.png';
+import serverflic from '../../assets/HTTP Server Flic.png';
+import websiteflic from '../../assets/website flic.png';
+
+
+const Projects = () => {
+
+  return (
+    <div>
+      <div className="mysite_projects" id="projects">
+        <h1 className="projects_name"> Projects</h1>
+        <div className="projects_row1">
+          <div className="spotify_project">
+            <a className="spotify_img" href="https://github.com/jacobireland/Spotify-API-Playlist-Creator" target= "_blank" rel="noreferrer"><img src={spotifyflic} width="300px" alt="spotify API project" /></a>
+            <h1 className="spotify_name"> Playlist Generator</h1>
+            <h1 className="spotify_blurb"> Uses the Spotify API to visualize a user’s top genres and create recommended playlists specific to their music taste.</h1>
+          </div>
+          <div className="webserver_project">
+            <h1 className="webserver_private"> Unfortunately, due to course policies the code for this project is private and only available upon request. If you’re interested in viewing the full project, please reach out and I can give you access.</h1>
+            <img className="webserver_img" src={serverflic} width="309px" alt="web server"/>
+            <h1 className="webserver_name"> HTTP Web Server</h1>
+            <h1 className="webserver_blurb"> Written in C, this is a simple HTTP web server that serves static HTML and image files and implements specific dynamic content.</h1>
+          </div>
+          <div className="website_project">
+            <a className="website_img" href="https://github.com/jacobireland/My-Website" target= "_blank" rel="noreferrer"><img src={websiteflic} width="311px" alt="spotify API project" /></a>
+            <h1 className="website_name"> My Website</h1>
+            <h1 className="website_blurb"> This website! I coded this from scratch using HTML, Javascript, CSS, and React JS.</h1>
+          </div>
+        </div>
+        <div className="projects_row2">
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Projects
